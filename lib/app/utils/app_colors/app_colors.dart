4f542f509c9bcfb8862c28a7b56286e91eee9561
masum <promotionbd321@gmail.com>
2025-05-01 17:22:04 +0600

    start

--- lib/app/utils/app_colors/app_colors.dart
+++ lib/app/utils/app_colors/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   AppColors._();
   ///============================ Red Series ===============================
 
-  // static const Color gray500 = Color(0xff333333);
+  static const Color black = Color(0xff171718);
 
 
 
--- lib/app/utils/app_strings/app_strings.dart
+++ lib/app/utils/app_strings/app_strings.dart
@@ -1,8 +1,9 @@
 class AppStrings {
   AppStrings._();
 
-  ///: <<<<<<======🗄️🗄️🗄️🗄️🗄️🗄️💡💡Authentication💡💡🗄️🗄️🗄️🗄️🗄️🗄️🗄️>>>>>>>>===========
-  static const signUp = "Sign up";
-
-
+  ///: <<<<<<======🗄️🗄️🗄️🗄️🗄️🗄️💡💡Onboard💡💡🗄️🗄️🗄️🗄️🗄️🗄️🗄️>>>>>>>>===========
+  static const signUp = "GPS Tracking";
+  static const trackYourLovedOnes =
+      "Track your loved ones, vehicles, or deliveries in real-time with our advanced GPS Tracking App.Whether you need to monitor your fleet, ensure child safety";
 }
+
