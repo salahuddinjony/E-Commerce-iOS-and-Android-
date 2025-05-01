92dc1c17285a240f138f08b1f71f66349a8ac363
masum <promotionbd321@gmail.com>
2025-05-01 18:43:52 +0600

    all string collect

--- lib/app/utils/app_strings/app_strings.dart
+++ lib/app/utils/app_strings/app_strings.dart
@@ -2,8 +2,115 @@ class AppStrings {
   AppStrings._();
 
   ///: <<<<<<======🗄️🗄️🗄️🗄️🗄️🗄️💡💡Onboard💡💡🗄️🗄️🗄️🗄️🗄️🗄️🗄️>>>>>>>>===========
-  static const signUp = "GPS Tracking";
+  static const gpsTracking = "GPS Tracking";
   static const trackYourLovedOnes =
       "Track your loved ones, vehicles, or deliveries in real-time with our advanced GPS Tracking App.Whether you need to monitor your fleet, ensure child safety";
+  static const needSomeTingDelivered = "Need something delivered ASAP? With Quick Delivery, get your packages,   or essentials delivered within minutes!";
+  static const quickDelivery = "Quick Delivery";
+  static const continues = "Continue";
+  ///: <<<<<<======🗄️🗄️🗄️🗄️🗄️🗄️💡💡Auth💡💡🗄️🗄️🗄️🗄️🗄️🗄️🗄️>>>>>>>>===========
+
+  static const logIn = "Log In";
+  static const newAccount = "New Account";
+  static const or = "Or";
+  static const signUpWithGoogle = "Sign Up with Google";
+  static const signUpWithApple = "Sign up with Apple";
+  static const createAccount = "Create Account";
+  static const registerToGetStarted = "Register to get started";
+  static const client = "Client";
+  static const businessVendor = "Business Vendor";
+  static const name = "Name";
+  static const emailOr = "E-mail/phone number";
+  static const password = "Password";
+  static const confirmPassword = "Confirm Password";
+  static const alreadyHaveAnAccount = "Already have an account?";
+  static const yourEmail = "Your Email";
+  static const forgotPassword = "Forgot Password";
+  static const dontHaveAnAccount = "Don’t have an account?";
+  static const signUp = "Sign up";
+  static const pleaseEnterYourEmailToReset = "Please enter your email to reset the password";
+  static const enterYourEmail = "Enter your email";
+  static const resetPassword  = "Reset Password";
+  static const checkYourEmail  = "Check your email";
+  static const weSendAReset  = "We sent a reset link to ";
+  static const verifyCode  = "Verify Code";
+  static const haveEntGotTheEmail  = "Haven’t got the email yet?";
+  static const resendEmail   = "Resend email";
+  static const setANewPassword   = "Set a new password";
+  static const createANewPassword   = "Create a new password. Ensure it differs from purposes ones for security";
+  static const enterYourNewPassword   = "Enter your new password";
+  static const reEnterPassword   = "Re-enter password";
+  static const updatePassword    = "Update Password";
+  ///: <<<<<<======🗄️🗄️🗄️🗄️🗄️🗄️💡💡Home💡💡🗄️🗄️🗄️🗄️🗄️🗄️🗄️>>>>>>>>===========
+
+  static const deliveryLocation    = "Delivery Location";
+  static const yourLocation    = "Your Location";
+  static const welcomeToTheCustomHUb    = "Welcome to the Custom Hub! Upload your design, choose colors, and personalize your T-shirt exactly how you want.";
+  static const customHubDesign    = "Custom Hub – Design Your Own Tee";
+  static const customHub    = "Custom Hub";
+  static const browseStores    = "Browse Stores";
+  static const discoverUniqueT    = "Discover Unique T-Shirt Stores on U TEE HUB!";
+  static const viewMap    = "View Map";
+  static const location    = "Location:";
+  static const home    = "Home";
+  static const order    = "Order";
+  static const chat    = "Chat";
+  static const support    = "Support";
+  static const aboutUs    = "About us";
+  static const privacyPolicy    = "Privacy policy";
+  static const termsOfService    = "Terms of Service";
+  ///: <<<<<<======🗄️🗄️🗄️🗄️🗄️🗄️💡💡Profile💡💡🗄️🗄️🗄️🗄️🗄️🗄️🗄️>>>>>>>>===========
+
+  static const profile    = "Profile";
+  static const editProfile    = "Edit Profile";
+  static const paymentMethod    = "Payment Methods";
+  static const orderHistory    = "Order History";
+  static const faq    = "FAQ";
+  static const settings    = "Settings";
+  static const logOut    = "Logout";
+  static const areYouSureWantToLogOut    = "Are you sure you want to log out ?";
+  static const no    = "No";
+  static const yes    = "Yes";
+  static const fullName    = "Full Name";
+  static const gender    = "Gender";
+  static const phone    = "Phone";
+  static const email    = "Email";
+  static const save    = "Save";
+  static const qrCode    = "QR Code";
+  static const howCanWeHelp    = "How can we help you?";
+  static const category    = "Category";
+  static const yourUTeeHub    = "Your U Tee Hub Account";
+  static const accountSecurity    = "Account Security";
+  static const orderManagement    = "Order Management";
+  static const contactUs     = "Contact US";
+  static const changePassword     = "Change password";
+  static const currentPassword     = "Current Password:";
+  static const newPassword     = "New Password:";
+  static const repeatPassword     = "Repeat Password:";
+  static const cancel      = "Cancel";
+
+  //
+  static const chatList      = "Chat List";
+  static const rating      = "Rating: ";
+  static const allTeeShirtDesigner      = "All Tee-Shirt Designer";
+  static const portfolio      = "Portfolio";
+  static const projectName      = "Project Name";
+  static const designType      = "Design Type";
+  static const popularTShirt      = "Popular T-Shirt Designs";
+  static const ourTopTeeShirt      = "Our Top Tee-Shirt Designer";
+  static const viewAll      = "View All";
+  static const shopDetails      = "Shop Details";
+  static const productAvailable      = "Products Available";
+  static const avarageRating      = "Average Rating: ";
+  static const totalEarning      = " Total Earnings ";
+  static const totalOrders      = "Total Orders ";
+  static const pendingOrders      = "Pending Orders";
+  static const completeOrders      = " Completed Orders";
+  static const viewOrder      = "view order";
+  static const bestSellingT      = "Best-Selling T-Shirts";
+  static const stockAlertBadgeLow      = "Stock Alert Badge (Low Stock 🔴)";
+
+
+
 }
 
