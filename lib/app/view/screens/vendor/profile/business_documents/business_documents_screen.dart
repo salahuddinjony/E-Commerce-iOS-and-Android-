d0f5598593dd5f1a012e02e966ed821f3d55c216
masum <promotionbd321@gmail.com>
2025-05-20 17:47:48 +0600

    fix this

--- lib/app/view/screens/vendor/profile/business_documents/business_documents_screen.dart
+++ lib/app/view/screens/vendor/profile/business_documents/business_documents_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:local/app/utils/app_colors/app_colors.dart';
 import 'package:local/app/view/common_widgets/custom_appbar/custom_appbar.dart';
+import 'package:local/app/view/common_widgets/custom_text/custom_text.dart';
 
 class BusinessDocumentsScreen extends StatelessWidget {
   const BusinessDocumentsScreen({super.key});
@@ -10,15 +11,39 @@ class BusinessDocumentsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      appBar: CustomAppBar(
+      appBar: const CustomAppBar(
         appBarContent: "Business Documents ",
         iconData: Icons.arrow_back,
       ),
       body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 20.h,vertical: 10.w),
+        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.w),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            CustomText(
+              text: "1. Upload Business License & Tax Info",
+              font: CustomFont.poppins,
+              fontWeight: FontWeight.w400,
+              fontSize: 18.sp,
+              color: AppColors.darkNaturalGray,
+            ),
+            SizedBox(height: 21.h,),
+            CustomText(
+              text: "2. Bank Account Details for Payouts",
+              font: CustomFont.poppins,
+              fontWeight: FontWeight.w400,
+              fontSize: 18.sp,
+              color: AppColors.darkNaturalGray,
+            ),
 
+            SizedBox(height: 21.h,),
+            CustomText(
+              text: "3. Withdrawal History & Earnings Report",
+              font: CustomFont.poppins,
+              fontWeight: FontWeight.w400,
+              fontSize: 18.sp,
+              color: AppColors.darkNaturalGray,
+            ),
           ],
         ),
       ),
