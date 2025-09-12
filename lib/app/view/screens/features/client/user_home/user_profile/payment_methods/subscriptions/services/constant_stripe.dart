88b1f3b0de7053cbb38334e3d8ff1a1b20e02137
salahuddinjony <salahuddin0758@gmail.com>
2025-09-12 16:48:22 +0600

    payment method added

---
+++ lib/app/view/screens/features/client/user_home/user_profile/payment_methods/subscriptions/services/constant_stripe.dart
@@ -0,0 +1,6 @@
+class Constantstripe {
+  static String stripePublishableKey =
+      "pk_test_51RMbgrQDnQq6pNb3Y8G8L46N0W3L1GKccJI2wOUTHr6gzqZlcJipIUY0BUuMWnSlCODZvQtmhltOHH85bY82HRyg00GEHMsR2m";
+  static String stripeSecretKey =
+      "sk_test_51RMbgrQDnQq6pNb3fjnPTVsPX3xCvU1EQgDIi6yWbo0tFxoxgZVjCudLBGz2AXUgTPcbb6Ptvd6qiNNHNEDxauxt00Yjgq8l41";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:local/app/global/helper/device_utils/device_utils.dart';
+import 'package:local/app/view/screens/features/client/user_home/user_profile/payment_methods/subscriptions/services/stripe_service.dart';
 
 import 'my_app.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  await StripeServicePayment.setup();
   DeviceUtils.lockDevicePortrait();
   runApp(const MyApp());
 }
